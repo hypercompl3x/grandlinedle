cab82c090c14f10ae825a63e5be64462649ef4d5
hypercompl3x <45500254+hypercompl3x@users.noreply.github.com>
2024-05-05 23:07:57 +0100

    added rate limitting

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,34 @@
+import { error, type Handle } from '@sveltejs/kit';
 import { createServerClient } from '@supabase/ssr';
-import { type Handle } from '@sveltejs/kit';
+import { Ratelimit } from '@upstash/ratelimit';
+import { createClient } from '@vercel/kv';
 import type { Database } from '$lib/types/DatabaseTypes';
 
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_API_KEY } from '$env/static/public';
+import { KV_REST_API_URL, KV_REST_API_TOKEN } from '$env/static/private';
+
+const kv = createClient({
+	url: KV_REST_API_URL,
+	token: KV_REST_API_TOKEN,
+});
+
+const ratelimit = new Ratelimit({
+	redis: kv,
+	limiter: Ratelimit.slidingWindow(5, '10 s'),
+});
 
 export const handle: Handle = async ({ event, resolve }) => {
+	const ip = event.getClientAddress() || '127.0.0.1';
+
+	const { success, reset } = await ratelimit.limit(ip);
+
+	if (!success) {
+		const timeRemaining = Math.floor((reset - new Date().getTime()) / 1000);
+		error(429, {
+			message: `Too many requests. Please try again in ${timeRemaining} seconds.`,
+		});
+	}
+
 	event.locals.supabase = createServerClient<Database>(
 		PUBLIC_SUPABASE_URL,
 		PUBLIC_SUPABASE_API_KEY,

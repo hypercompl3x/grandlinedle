a1b45b5421a49c276c1330a22ea82022e6377443
hypercompl3x <45500254+hypercompl3x@users.noreply.github.com>
2026-07-02 23:02:27 +0100

    new easter egg character

--- src/lib/services/characterService.ts
+++ src/lib/services/characterService.ts
@@ -21,3 +21,22 @@ export const getCharactersFromQuery = async (query: string, currentGuesses: Char
 
 	return charactersWithImages;
 };
+
+export const getEasterEggCharacters = async (currentGuesses: Character['id'][]) => {
+	const { data: characters, error } = await supabase
+		.from('characters')
+		.select()
+		.gt('id', 9000)
+		.not('id', 'in', `(${currentGuesses.join(',')})`);
+
+	if (error) {
+		console.error(error.message);
+		return [];
+	}
+
+	const charactersWithImages = await getImages(characters, supabase, 'characters');
+
+	await Promise.all(charactersWithImages.map(i => preloadImage(i.url)));
+
+	return charactersWithImages;
+};
